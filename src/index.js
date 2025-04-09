6f75dc2fe31d07a705c808e2e4a6c86bbe3745f5
Kaspar Bergert <kasparbergert6@gmail.com>
2025-04-09 18:37:37 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // enables jsx.
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//jsx --
+//  a javscript file
+//  that is suited for react syntax.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(document.getElementById("root"));
+  
+let elementP = React.createElement('div', null, "HELLO KASPAR");
+root.render(elementP);
+
