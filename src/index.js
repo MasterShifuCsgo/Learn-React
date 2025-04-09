419b59f6d5890ed4baf30d03e8e53dffb0541405
Kaspar Bergert <kasparbergert6@gmail.com>
2025-04-09 18:42:11 +0300

    react function usage.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from "react"; // enables jsx.
 import { createRoot } from "react-dom/client";
 
+function MyApp(){
+  return React.createElement('ul', null, [
+    React.createElement('li', null, "TextContent"),
+    React.createElement('li', null, "TextContent"),
+    React.createElement('li', null, "TextContenat"),
+  ]);
+}
+
 //jsx --
 //  a javscript file
 //  that is suited for react syntax.
 
 const root = createRoot(document.getElementById("root"));
-  
-let ul = React.createElement('ul', null, [
-  React.createElement('li', null, "TextContent"),
-  React.createElement('li', null, "TextContent"),
-  React.createElement('li', null, "TextContent"),
-]);
 
-
+let ul = MyApp();
 
 root.render(ul);
 
