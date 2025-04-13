7622edd6c09998b6be6ef68bfeb7b9e9bc7a43e1
Kaspar Bergert <kasparbergert6@gmail.com>
2025-04-13 11:29:26 +0300

    exercise 5

--- src/App.js
+++ src/App.js
@@ -1,73 +1,38 @@
 import React from "react"
-import TodoItem from './components/TodoItem' 
-import TodoItemList from './TodoItemList.js'
+
+
 
 class App extends React.Component {
   constructor(){
     super()
     this.state = {
-        todos: TodoItemList
-     }
-  }
-
-  handleChange(changedItemId){
-    this.setState(prevState => {
-      const updatedState = prevState.todos.map((item) => {        
-        return item.id === changedItemId 
-          ? {...item, completed: !item.completed}
-          : item;
-      })
-      return {todos: updatedState}
-    });
-
-  }
-
+      loggedIn: false,
+    }
   
-  static getDerivedStateFromProps(){
-    // return the new updated state based on props.
-    // only used to set its own state based upon the sent props.
-    // do not use this.
-  }
-
-  getSnapshotBeforeUpdate(){
-    // captures dom state.  
-  }
-
-  componentDidMount(){
-      // runs when render is first called
-      // GET the data i need to correctly display
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    // runs when react thinks it should update the component, even though nothing changed about it.
-    // this is used to optimize the website. Tells react whether is even important to update the component.
-
-    // return true if you want to update, false when you dont.
-  }
-
-  componentWillUnmount(){
-    // what happens when the component unmounts.
-    // used to clean up the component when unmounting.
-    // for example, remove event listeners.
-
+    this.logUserIn = this.logUserIn.bind(this);
 
   }
 
-  render(){
+  logUserIn(){
+    this.setState((prevState) => {
+      let newState = { loggedIn : !prevState.loggedIn};
+      return newState;
+    })
+  }
 
-    let todoItems = this.state.todos.map(obj => (
-       <TodoItem 
-       id={obj.id} 
-       desc={obj.desc} 
-       completed={obj.completed} 
-       handleChange={ (changedItemId) => this.handleChange(changedItemId)}>        
-       </TodoItem>
-       ));
+  render(){ 
 
-    return(
-      <div>
-        {todoItems}
-      </div>
+    const {loggedIn} = this.state;
+    
+    const displayText = loggedIn ? "Logged in": "Logged out";
+    const displayButtonText = loggedIn ? "Log out" : "Log in";
+    return (
+      <div>      
+        <h1> {displayText} </h1>
+        <button onClick={this.logUserIn}>
+        {displayButtonText}
+        </button>
+      </div>  
     )
 
   } 
