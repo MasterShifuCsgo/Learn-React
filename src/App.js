17f859ae61cdf3fa289c0cf6fd812a129d3de20d
Kaspar Bergert <kasparbergert6@gmail.com>
2025-04-13 10:37:00 +0300

    lifecycle methods

--- src/App.js
+++ src/App.js
@@ -22,6 +22,26 @@ class App extends React.Component {
 
   }
 
+  componentDidMount(){
+      // runs when render is first called
+      // GET the data i need to correctly display
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    // runs when react thinks it should update the component, even though nothing changed about it.
+    // this is used to optimize the website. Tells react whether is even important to update the component.
+
+    // return true if you want to update, false when you dont.
+  }
+
+  componentWillUnmount(){
+    // what happens when the component unmounts.
+    // used to clean up the component when unmounting.
+    // for example, remove event listeners.
+
+
+  }
+
   render(){
 
     let todoItems = this.state.todos.map(obj => (
